4e0e287b3c2c2df7ddf2f9d9f9b6f86cda1cd209
Thorsten Ruppenstein <th_ru@web.de>
2024-01-30 19:03:31 +0100

    hack to show the better gaining stats for ghost axe, flint, etc. in weapon description

---
+++ extensions/effects/gain_stat_every_killed_enemies_effect.gd
@@ -0,0 +1,19 @@
+extends "res://effects/weapons/gain_stat_every_killed_enemies_effect.gd"
+
+func get_args()->Array:
+	
+	var ghost_crown_effect = RunData.effects['ghost_crown_effect']
+	var ghost_cape_effect = RunData.effects['ghost_cape_effect']
+	
+	var adjusted_value = value - ghost_crown_effect
+	
+	if adjusted_value < 1:
+		adjusted_value = 1
+	
+	var str_stat_nb = str(stat_nb + ghost_cape_effect)
+	var translated_stat = tr(stat.to_upper())
+	var str_value = str(adjusted_value)
+	
+	
+	return [str_stat_nb, translated_stat, str_value]
+
