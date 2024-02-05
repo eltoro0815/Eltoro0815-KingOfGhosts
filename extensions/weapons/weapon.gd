d6d0ae8319281545055d7316896e918e9b8cfec7
Thorsten Ruppenstein <th_ru@web.de>
2024-02-05 18:48:25 +0100

    changed progress visualization of sword

--- extensions/weapons/weapon.gd
+++ extensions/weapons/weapon.gd
@@ -2,14 +2,21 @@ extends "res://weapons/weapon.gd"
 
 
 var image_number = 0
-var kills_counter = 0
+var kills_counter_hp = 0
+
 var img_ghost_sword_blood = [
-	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_0.png"),
-	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_1.png"),
-	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_2.png"),
-	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_3.png"),
-	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_4.png"),
-	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_5.png")
+	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_0_1.png"), #0
+	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_1_1.png"), #1
+	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_2_1.png"), #2
+	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_3_1.png"), #3
+	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_4_1.png"), #4
+	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_5_1.png"), #5
+	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_0_2.png"), #6
+	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_1_2.png"), #7
+	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_2_2.png"), #8
+	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_3_2.png"), #9
+	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_4_2.png"), #10
+	load("res://mods-unpacked/Eltoro0815-KingOfGhosts/overwrites/eltoro0815_ghost_sword_blood_5_2.png")  #11
 ]
 
 	
@@ -20,22 +27,36 @@ func update_ghost_sword()->void :
 
 func map_percentage_to_values(percent: float) -> int:
 	var index = 0
-	if percent >= 0.1:
+	if percent >= 0.05:
 		index = 1
-	if percent >= 0.3:
+	if percent >= 0.15:
 		index = 2
-	if percent >= 0.5:
+	if percent >= 0.25:
 		index = 3
-	if percent >= 0.7:
+	if percent >= 0.35:
 		index = 4
-	if percent >= 0.9:
+	if percent >= 0.45:
 		index = 5
-
+	if percent >= 0.5:
+		index = 6
+	if percent >= 0.55:
+		index = 7
+	if percent >= 0.65:
+		index = 8
+	if percent >= 0.75:
+		index = 9
+	if percent >= 0.85:
+		index = 10
+	if percent >= 0.95:
+		index = 11
+	
+	
 	return index
 
 
 func get_image_number_from_kills(actual_kills, needed_kills) :
 	var percent_value : float = float(actual_kills)/float(needed_kills)
+	
 	return map_percentage_to_values(percent_value)
 
 	
@@ -53,7 +74,7 @@ func on_killed_something(_thing_killed:Node)->void :
 
 			if weapon_id == "weapon_ghost_sword" :
 				if effect.custom_key == "ghost_sword_blood":
-					kills_counter += 1
+					kills_counter_hp += 1
 			
 			var effect_value = 0
 			
@@ -70,14 +91,14 @@ func on_killed_something(_thing_killed:Node)->void :
 			if nb_enemies_killed_this_wave % effect_value == 0:
 				if effect.custom_key == "ghost_sword_blood":
 					image_number = 0
-					kills_counter = 0
+					kills_counter_hp = 0
 					update_ghost_sword()
 					
 				RunData.add_stat(effect.stat, effect.stat_nb + ghost_cape_effect)
 				emit_signal("tracked_value_updated")
 			else :
 				if effect.custom_key == "ghost_sword_blood":
-					var new_image_number = get_image_number_from_kills(kills_counter, effect_value)
+					var new_image_number = get_image_number_from_kills(kills_counter_hp, effect_value)
 					if new_image_number != image_number:
 						image_number = new_image_number
 						update_ghost_sword()
