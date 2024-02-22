1add6420b4dfdcb4846dba2ed4a3db345664afe4
Thorsten Ruppenstein <th_ru@web.de>
2024-02-22 22:10:54 +0100

    let the sword be more dependent on how many weird ghosts you have

--- extensions/main.gd
+++ extensions/main.gd
@@ -16,30 +16,60 @@ func on_levelled_up()->void :
 	
 	var level = RunData.current_level
 	
-	if level == 10:
+	if level == 1 or level == 10:
 		var character = RunData.current_character
 		if character.my_id == "character_ghostking_desc":
-			var weird_ghost_item = ItemService.get_element(ItemService.items, "item_weird_ghost")
-		
-			if weird_ghost_item != null:
-				RunData.add_item(weird_ghost_item)
-				
+			add_n_weird_ghosts(1)
 				
 	if level == 20:
 		var character = RunData.current_character
 		if character.my_id == "character_ghostking_desc":
-			var weird_ghost_item = ItemService.get_element(ItemService.items, "item_weird_ghost")
-		
-			if weird_ghost_item != null:
-				RunData.add_item(weird_ghost_item)
-				RunData.add_item(weird_ghost_item)
+			add_n_weird_ghosts(2)
 				
 	if level == 30:
 		var character = RunData.current_character
 		if character.my_id == "character_ghostking_desc":
-			var weird_ghost_item = ItemService.get_element(ItemService.items, "item_weird_ghost")
-		
-			if weird_ghost_item != null:
-				RunData.add_item(weird_ghost_item)
-				RunData.add_item(weird_ghost_item)
-				RunData.add_item(weird_ghost_item)
+			add_n_weird_ghosts(3)
+	
+	if level == 40:
+		var character = RunData.current_character
+		if character.my_id == "character_ghostking_desc":
+			add_n_weird_ghosts(5)
+			
+	if level == 50:
+		var character = RunData.current_character
+		if character.my_id == "character_ghostking_desc":
+			add_n_weird_ghosts(8)
+			
+	if level == 60:
+		var character = RunData.current_character
+		if character.my_id == "character_ghostking_desc":
+			add_n_weird_ghosts(13)
+			
+	if level == 70:
+		var character = RunData.current_character
+		if character.my_id == "character_ghostking_desc":
+			add_n_weird_ghosts(21)
+			
+	if level == 80:
+		var character = RunData.current_character
+		if character.my_id == "character_ghostking_desc":
+			add_n_weird_ghosts(34)
+			
+	if level == 90:
+		var character = RunData.current_character
+		if character.my_id == "character_ghostking_desc":
+			add_n_weird_ghosts(55)
+			
+	if level == 100:
+		var character = RunData.current_character
+		if character.my_id == "character_ghostking_desc":
+			add_n_weird_ghosts(89)
+
+
+
+func add_n_weird_ghosts(n: int):
+	var weird_ghost_item = ItemService.get_element(ItemService.items, "item_weird_ghost")
+	if weird_ghost_item != null:
+		for i in range(0,n): # i gets the values 0,1, ..., n-1 (weird syntax^^)
+			RunData.add_item(weird_ghost_item)
