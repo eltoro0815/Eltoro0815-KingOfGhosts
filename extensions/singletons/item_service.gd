8f8031f1c54a0d344983a072295f973ae35cb852
Thorsten Ruppenstein <th_ru@web.de>
2024-05-25 22:14:02 +0200

    better wording for rainbow ghost effect

--- extensions/singletons/item_service.gd
+++ extensions/singletons/item_service.gd
@@ -4,7 +4,7 @@ func get_value(wave:int, base_value:int, affected_by_items_price_stat:bool = tru
 
 	var value = .get_value(wave, base_value, affected_by_items_price_stat, is_weapon, item_id)
 
-	# if you have the rainbow ghost the price for a weapon is capped by the rainbow_ghost_effect
+	# if you have the rainbow ghost the price for a weapon is capped at the value of the rainbow_ghost_effect
 	if is_weapon:
 		var rainbow_ghost_effect = RunData.effects['rainbow_ghost_effect']
 		if int(rainbow_ghost_effect) > 0:
